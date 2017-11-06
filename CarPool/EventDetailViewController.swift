b5452cd3031541992ea57b4e18d7d685309d4bbd
Riyazh Dholakia <theAIRBEAST@Riyazhs-MacBook-Air.local>
2017-11-06 15:04:34 -0500

    Force commit

--- CarPool/EventDetailViewController.swift
+++ CarPool/EventDetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CarpoolKit
 
 class EventDetailViewController: UIViewController {
     
--- CarPool/RootViewController.swift
+++ CarPool/RootViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class RootViewController: UITableViewController {
 
     override func viewDidLoad() {
