6c25f574a0c7af1a06e8a990b37650de0ff7041a
Riyazh Dholakia <theAIRBEAST@Riyazhs-MacBook-Air.local>
2017-11-06 15:45:29 -0500

    Create a CreateTripViewController

---
+++ CarPool/CreateTripViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CreateTripViewController.swift
+//  CarPool
+//
+//  Created by Riyazh Dholakia on 11/6/17.
+//  Copyright © 2017 Riyazh. All rights reserved.
+//
+
+import UIKit
+
+class CreateTripViewController: UIViewController {
+    
+}
