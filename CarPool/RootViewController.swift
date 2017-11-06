e99239b6a2f60e20466d982328ec181d5a419824
Naina <Naina@Nainas-Macbook-Air.local>
2017-11-06 15:03:13 -0500

    Set up RootVC

--- CarPool/RootViewController.swift
+++ CarPool/RootViewController.swift
@@ -7,16 +7,28 @@
 //
 
 import UIKit
+import CarpoolKit
 
 class RootViewController: UITableViewController {
+
+    var events: [String] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return events.count
+        
+    }
     
-
-
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "A", for: indexPath)
+        cell.textLabel?.text = events[indexPath.row]
+        return cell
+    }
+    
 }
 
