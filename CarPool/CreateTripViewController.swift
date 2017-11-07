af563ce66d514934b0106881b61d5a528dbca697
Riyazh Dholakia <theAIRBEAST@Riyazhs-MacBook-Air.local>
2017-11-07 12:29:48 -0500

    add new createVC objects

--- CarPool/CreateTripViewController.swift
+++ CarPool/CreateTripViewController.swift
@@ -13,5 +13,13 @@ class CreateTripViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        createTrip()
+    }
+    
+    func createTrip() {
+        API.createTrip(eventDescription: <#T##String#>, eventTime: <#T##Date#>, eventLocation: <#T##CLLocation#>) { (<#Trip#>) in
+            <#code#>
+        }
     }
 }
