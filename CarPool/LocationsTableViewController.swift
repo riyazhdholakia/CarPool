3a2652ba6718f42c0601e49c442e53847806d4eb
Naina <Naina@Nainas-Macbook-Air.local>
2017-11-08 14:32:10 -0500

    Added MapVC and LocationsVC

---
+++ CarPool/LocationsTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LocationsTableViewController.swift
+//  CarPool
+//
+//  Created by Naina  on 11/8/17.
+//  Copyright © 2017 Riyazh. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CarpoolKit
+
+class LocationsTableViewController: UITableViewController {
+    var location: [String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return location.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Locations", for: indexPath)
+        cell.textLabel?.text = location[indexPath.row]
+        return cell
+    }
+}
+
+
---
+++ CarPool/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  CarPool
+//
+//  Created by Naina  on 11/8/17.
+//  Copyright © 2017 Riyazh. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+    class MapViewController: UIViewController {
+        
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+        }
+}
